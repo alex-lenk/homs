423c08b19062365b565d3ec37e28c1885938c2f6
AlexLenk <alexlenk.ru@gmail.com>
2020-01-01 16:12:05 +0300

    Created advantages blocks

--- src/js/lib.js
+++ src/js/lib.js
@@ -2,6 +2,7 @@
 //= lib/svgxuse.min.js
 //= lib/bootstrap-4.4.1/util.js
 //= lib/bootstrap-4.4.1/scrollspy.js
-//= lib/stickUp.js
+//= lib/bootstrap-4.4.1/collapse.js
+// = lib/stickUp.js
 // = ../../node_modules/@fancyapps/fancybox/dist/jquery.fancybox.min.js
 // = lib/jquery.custom-scroll.min.js
