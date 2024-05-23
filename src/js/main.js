bb06c0ed1105172cdf7334b129f6364e2d0c0c60
AlexLenk <alexlenk.ru@gmail.com>
2024-05-23 08:11:23 +0300

    Created reports component

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,14 @@
+import { Fancybox } from '@fancyapps/ui';
+
 import initNavbarPanel from './components/initNavbarPanel.js';
 import initSolutionSlider from './components/initSolutionSlider.js';
+import initReportsSlider from './components/initReportsSlider.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   initNavbarPanel();
   initSolutionSlider();
+  initReportsSlider();
+
+  // init Fancybox
+  Fancybox.bind('[data-fancybox="gallery"]', {});
 });
