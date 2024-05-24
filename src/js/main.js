59e6ef7e63d87a86a131d52c648190d8cc71eb26
AlexLenk <alexlenk.ru@gmail.com>
2024-05-24 09:35:29 +0300

    Created footer and modal components

--- src/js/main.js
+++ src/js/main.js
@@ -4,12 +4,16 @@ import initNavbarPanel from './components/initNavbarPanel.js';
 import initSolutionSlider from './components/initSolutionSlider.js';
 import initReportsSlider from './components/initReportsSlider.js';
 import initCreatedSlider from './components/initCreatedSlider.js';
+import initializeModals from './components/initializeModals.js';
+import initTextareaLimit from './components/initTextareaLimit.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   initNavbarPanel();
   initSolutionSlider();
   initReportsSlider();
   initCreatedSlider();
+  initializeModals();
+  initTextareaLimit();
 
   // init Fancybox
   Fancybox.bind('[data-fancybox]', {});
